bfc5b8db84799fa421275dc0cd979962ffc6f34f
shivzee <shivampurwar04@gmail.com>
2021-09-15 22:38:44 +0530

    Code for springboot-zuul-test

---
+++ src/main/java/com/test/app/SimpleFilter.java
@@ -0,0 +1,40 @@
+package com.test.app;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+public class SimpleFilter extends ZuulFilter {
+
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		if (!"ALTIMETRIK".equals(request.getHeader("client-path"))) {
+			ctx.unset();
+			ctx.setResponseStatusCode(HttpStatus.FORBIDDEN.value());
+		}
+
+		return null;
+	}
+
+}
